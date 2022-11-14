959d996eb5fb6e195d595cc1ec2618ddb804ff65
AndroidLion48 <>
2022-11-14 11:03:31 -0800

    Passed Arguments into nav_graph.xml

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -1,7 +1,6 @@
 package com.example.wordsapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -9,6 +8,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,29 +16,27 @@ import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-private lateinit var recyclerView: RecyclerView
-private var isLinearLayoutManager = true
-private var _binding: FragmentLetterListBinding? = null
-private val binding get() = _binding!!
-
 /**
  * A simple [Fragment] subclass.
  * Use the [LetterListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class LetterListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+
     // Keeps track of which LayoutManager is in use for the [RecyclerView]
+    private var isLinearLayoutManager = true
+
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setHasOptionsMenu(true)
-
-        recyclerView = binding.recyclerView
-        // Sets the LinearLayoutManager of the recyclerview
-        chooseLayout()
     }
 
     override fun onCreateView(
@@ -46,18 +44,19 @@ class LetterListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Retrieve and inflate the layout for this fragment
         _binding = FragmentLetterListBinding.inflate(
             inflater,
             container,
-            false)
-        val view = binding.root
-        return view
+            false
+        )
+        return binding.root
     }
-
-    private lateinit var recyclerView: RecyclerView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.recyclerView
+        // Sets the LayoutManager of the recyclerview
+        // On the first run of the app, it will be LinearLayoutManager
         chooseLayout()
     }
 
@@ -74,16 +73,14 @@ class LetterListFragment : Fragment() {
     }
 
     private fun chooseLayout() {
-        when (isLinearLayoutManager) {
-            true -> {
-                com.example.wordsapp.recyclerView.layoutManager = LinearLayoutManager(context)
-                com.example.wordsapp.recyclerView.adapter = LetterAdapter()
-            }
-            false -> {
-                com.example.wordsapp.recyclerView.layoutManager = GridLayoutManager(context, 4)
-            }
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(context, 4)
         }
+        recyclerView.adapter = LetterAdapter()
     }
+
     private fun setIcon(menuItem: MenuItem?) {
         if (menuItem == null)
             return
@@ -91,7 +88,7 @@ class LetterListFragment : Fragment() {
         menuItem.icon =
             if (isLinearLayoutManager)
                 ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
-            else ContextCompat.getDrawable(this.requireContext(),R.drawable.ic_linear_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
